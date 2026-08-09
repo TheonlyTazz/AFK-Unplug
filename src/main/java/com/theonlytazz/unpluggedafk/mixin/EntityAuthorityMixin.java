acb40830581946644d64250381c662526903a747
Leroy <TheonlyTazz@gmail.com>
2026-08-09 20:44:08 +0200

    fix: preserve offline player physics authority

---
+++ src/main/java/com/theonlytazz/unpluggedafk/mixin/EntityAuthorityMixin.java
@@ -0,0 +1,19 @@
+package com.theonlytazz.unpluggedafk.mixin;
+
+import com.theonlytazz.unpluggedafk.player.OfflinePlayer;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+abstract class EntityAuthorityMixin {
+    @Inject(method = "isLocalInstanceAuthoritative", at = @At("HEAD"), cancellable = true)
+    private void unpluggedAfk$serverControlsOfflinePlayers(CallbackInfoReturnable<Boolean> callback) {
+        Entity self = (Entity) (Object) this;
+        if (self instanceof OfflinePlayer || self.getControllingPassenger() instanceof OfflinePlayer) {
+            callback.setReturnValue(!self.level().isClientSide());
+        }
+    }
+}
